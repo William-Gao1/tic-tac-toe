b0a0fc7b507d090d74fabf7bb74445505dba71f1
William Gao <williamgao@williams-mbp.lan>
2021-01-18 16:10:06 -0500

    added better gui

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -3,16 +3,55 @@ import pygame
 import math
 
 from pygame.locals import *
+import pygame.freetype
 pygame.init()
+pygame.font.init()
 
+WIDTH, HEIGHT = 500,500
+TOGGLE_BUTTON = (550, 450)
+RESET_BUTTON = (550, 400)
+screen = pygame.display.set_mode([700, 500])
 player = 0
 ai = 0
 mask = 0
+aiActive = True
 playerTurn = True
-screen = pygame.display.set_mode([500, 500])
-WIDTH, HEIGHT = screen.get_size()
-screen.fill((255, 255, 255))
+gameRunning = True
+GAME_FONT = pygame.freetype.Font("Calibri Regular.ttf", 24)
 
+
+def reset():
+    global screen
+    global player
+    global ai
+    global mask
+    global playerTurn
+    global gameRunning
+    player = 0
+    ai = 0
+    mask = 0
+    
+    playerTurn = True
+    gameRunning = True
+    screen = pygame.display.set_mode([700, 500])
+    
+    screen.fill((255, 255, 255))
+    
+    for i in range(4):
+        pygame.draw.line(screen, (0, 0, 0), (i*WIDTH/3, HEIGHT), (i*WIDTH/3, 0))
+        pygame.draw.line(screen, (0, 0, 0), (0, i * HEIGHT/3),
+                                (WIDTH, i * HEIGHT/3))
+
+
+    pygame.draw.rect(screen, (250,100,0), (TOGGLE_BUTTON[0], TOGGLE_BUTTON[1],125,25))
+    GAME_FONT.render_to(screen, (TOGGLE_BUTTON[0]+15, TOGGLE_BUTTON[1]+5), "Toggle AI", (0, 0, 0))
+    #textsurface, rect = myfont.render('Some Text', False, (0, 0, 0))
+
+
+    pygame.draw.rect(screen, (250,100,0), (RESET_BUTTON[0], RESET_BUTTON[1],125,25))
+    GAME_FONT.render_to(screen, (RESET_BUTTON[0]+35, RESET_BUTTON[1]+5), "Reset", (0, 0, 0))
+
+reset()
 
 def makeMove(pos, opp, mask, space):
     
@@ -23,10 +62,7 @@ def makeMove(pos, opp, mask, space):
 
 
 
-for i in range(3):
-    pygame.draw.line(screen, (0, 0, 0), (i*WIDTH/3, HEIGHT), (i*WIDTH/3, 0))
-    pygame.draw.line(screen, (0, 0, 0), (0, i * HEIGHT/3),
-                            (WIDTH, i * HEIGHT/3))
+
 
 
 def main():
@@ -34,6 +70,7 @@ def main():
     global player
     global mask
     global ai
+    global aiActive
 
     
     
@@ -47,26 +84,65 @@ def main():
                 x, y = pygame.mouse.get_pos()
                 
                 space = getSpace(x,y)
-                drawX(space) 
-                player, mask = makeMove(player, ai, mask, space)
-                checkState(player, mask)
+                if space != -1 and gameRunning and playerTurn:
+                    drawX(space) 
+                    player, mask = makeMove(player, ai, mask, space)
+                    state = checkState(player, mask)
                 
-                
+                    if state != -1:
+                        endGame(state)
+                        break
 
-                
-                aiMove = minimax (ai, player, mask, 20, -math.inf, math.inf, True)
-                print(aiMove)
-                #print(canPlay (0, mask))
-                drawO(aiMove[1])
-                ai, mask = makeMove(ai, player, mask, aiMove[1])
-                checkState(ai, mask)
+                    if aiActive:
+                        aiMove = minimax (ai, player, mask, 20, -math.inf, math.inf, True)
+                        drawO(aiMove[1])
+                        ai, mask = makeMove(ai, player, mask, aiMove[1])
+                    
+                        state = checkState(ai, mask)
+                        playerTurn =  not playerTurn
+                    
+                    if state != -1:
+                        endGame(state)
+                        break
 
-    
+                    playerTurn =  not playerTurn
+                elif space != -1 and gameRunning and  not playerTurn:
+                    drawO(space)
+                    ai, mask = makeMove(ai, player, mask, space)
+                    state = checkState(ai, mask)
+                    if state != -1:
+                        endGame(state)
+                        break
+                    playerTurn = not playerTurn
+                    
+                else:
+                    checkButtons(x, y)
+
+
+        GAME_FONT.render_to(screen, (TOGGLE_BUTTON[0]+15, TOGGLE_BUTTON[1]+5), "Toggle AI", (0, 0, 0))
+
         pygame.display.flip()
 
    
     pygame.quit()
 
+def checkButtons(x,y):
+    global aiActive
+    global playerTurn
+    global ai
+    global mask
+    global player
+    if x > RESET_BUTTON[0] and x < RESET_BUTTON[0] + 125 and y > RESET_BUTTON[1] and y < RESET_BUTTON[1]+25:
+        reset()
+    elif x > TOGGLE_BUTTON[0] and x < TOGGLE_BUTTON[0] + 125 and y > TOGGLE_BUTTON[1] and y < TOGGLE_BUTTON[1]+25:
+        aiActive =  not aiActive
+        if aiActive and not playerTurn:
+            aiMove = minimax (ai, player, mask, 20, -math.inf, math.inf, True)
+            drawO(aiMove[1])
+            ai, mask = makeMove(ai, player, mask, aiMove[1])
+                    
+            state = checkState(ai, mask)
+            playerTurn =  not playerTurn
 
 def drawX(space):
     x,y = getSpaceTopLeft(space)
@@ -78,6 +154,16 @@ def drawO(space):
     x,y = getSpaceCentre(space)
     pygame.draw.circle(screen, (0, 0, 0), (x,y), WIDTH/6, 10)
 
+def endGame(status):
+    global gameRunning
+    if status == 0:
+        GAME_FONT.render_to(screen, (575, 100), "Tie!", (0, 0, 0))
+    elif not playerTurn:
+        GAME_FONT.render_to(screen, (575, 100), "O Wins!", (0, 0, 0))
+    else:
+        GAME_FONT.render_to(screen, (575, 100), "X Wins!", (0, 0, 0))
+    gameRunning = False
+    
 
 def getSpaceTopLeft(space):
     return (space % 3) * (WIDTH/3), (space // 3) * (HEIGHT/3)
@@ -87,7 +173,10 @@ def getSpaceCentre(space):
 
 
 def getSpace(x, y):
-    return (x//(WIDTH//3)) + (3 * ((y//(HEIGHT//3))))
+    if x < WIDTH and y < HEIGHT:
+        return(x//(WIDTH//3)) + (3 * ((y//(HEIGHT//3))))
+    else:
+        return -1
 
 
 def checkState(pos, mask):
